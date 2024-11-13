d18ca3f5e6465249a44646f9153faa9633702e17
Alireza-Nikzad <www.alireza2215@gmail.com>
2024-11-13 20:41:55 +0330

    Update ConwaysGameOfLife.py

--- ConwaysGameOfLife.py
+++ ConwaysGameOfLife.py
@@ -1,8 +1,19 @@
 import tkinter as tk
+from tkinter import ttk
 
 def main():
     root = tk.Tk()
     root.geometry("1200x800")
+    root.wm_title("Conway's Game of Life")
+
+    frame = ttk.Frame(root)
+    frame.pack(fill=tk.BOTH, expand= True, ipadx=0, ipady= 0)
+    
+    canvas = tk.Canvas(frame)
+    canvas.pack(fill=tk.BOTH, expand=True, ipadx=0, ipady=0)
+    canvas.create_rectangle(0, 0, 1200, 800, fill='yellow')
+    
     root.mainloop()
+    
     
 main()
