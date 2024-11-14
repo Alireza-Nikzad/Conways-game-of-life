40b18dcfe671d133e67fba4e1d3bd1d03f0c66b4
Alireza-Nikzad <www.alireza2215@gmail.com>
2024-11-14 20:53:04 +0330

    refactoring into an OO structure

--- ConwaysGameOfLife.py
+++ ConwaysGameOfLife.py
@@ -1,19 +1,23 @@
 import tkinter as tk
 from tkinter import ttk
 
-def main():
-    root = tk.Tk()
-    root.geometry("1200x800")
-    root.wm_title("Conway's Game of Life")
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("1200x800")
+        self.wm_title("Conway's Game of Life")
+    
+class MainFrame(ttk.Frame):
+    def __init__(self, root):
+        super().__init__(root)
+        
+        self.pack(fill=tk.BOTH, expand= True, ipadx=0, ipady= 0)
+        canvas = tk.Canvas(self)
+        canvas.pack(fill=tk.BOTH, expand=True, ipadx=0, ipady=0)
+        canvas.create_rectangle(0, 0, 1200, 800, fill='blue')
 
-    frame = ttk.Frame(root)
-    frame.pack(fill=tk.BOTH, expand= True, ipadx=0, ipady= 0)
-    
-    canvas = tk.Canvas(frame)
-    canvas.pack(fill=tk.BOTH, expand=True, ipadx=0, ipady=0)
-    canvas.create_rectangle(0, 0, 1200, 800, fill='yellow')
-    
-    root.mainloop()
-    
-    
+def main():
+    app = App()
+    MainFrame(app)
+    app.mainloop()
 main()
