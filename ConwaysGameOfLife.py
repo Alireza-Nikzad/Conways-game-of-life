5f918fb7f042ba0efc4c9cfaa58810d70f3c58a9
Alireza-Nikzad <www.alireza2215@gmail.com>
2024-11-15 21:26:56 +0330

    Laying out widgets with grids

--- ConwaysGameOfLife.py
+++ ConwaysGameOfLife.py
@@ -12,9 +12,23 @@ class MainFrame(ttk.Frame):
         super().__init__(root)
         
         self.pack(fill=tk.BOTH, expand= True, ipadx=0, ipady= 0)
+        
+        self.columnconfigure(0,weight=1)
+        self.columnconfigure(1,weight=2000)
+        self.rowconfigure(0, weight=2000)
+        self.rowconfigure(1, weight=1)
+        
+        next_button= ttk.Button(self, text="Step")
+        next_button.grid(column=0, row=1, sticky=tk.W, padx=5, pady=1)
+        
+        clear_button= ttk.Button(self, text="Clear")
+        clear_button.grid(column=1, row=1, sticky=tk.W, padx=5, pady=1)
+        
+        
+        
         canvas = tk.Canvas(self)
-        canvas.pack(fill=tk.BOTH, expand=True, ipadx=0, ipady=0)
         canvas.create_rectangle(0, 0, 1200, 800, fill='blue')
+        canvas.grid(column=0, row=0, columnspan=2, sticky='NEWS', padx=5, pady=3)
 
 def main():
     app = App()
